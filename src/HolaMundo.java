8c1456f539e4ebc8efef35c502784031060a9670
unknown <jmdiazarjona@gmail.com>
2024-04-05 17:44:30 +0200

    segundo commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo {
+    //Debe ser public para que pueda ser interpretado por la JVM - MÃ¡quina Virtual Java
+    //Atajo : escribir main + enter o psvm + tab
+    //void es el tipo de retorno, void no retorna nada, solo es un proceso
+    public static void main(String[] args){
+        //proceso o tarea que queremos implementar
+        //parametros o argumentos mendiante terminal
+        String strName = "Juanma";
+        System.out.print( "Hola Lebrija desde Java, mi nombre es: " + strName);
+    }
+}
